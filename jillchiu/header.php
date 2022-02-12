39f98ac3aeb6ac01ab1e0b78d0b7ca17ecad1220
jillchiu <99075985+jillchiu@users.noreply.github.com>
2022-02-12 11:23:50 +0900

    Add files via upload

---
+++ jillchiu/header.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<!-- jQuery -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="script.js"></script>
+<header>
+	<div class="navbar_head">
+		
+		<div class="h_from_left_dropdown">
+			
+			<a href="index.php" class="dropdown_btn">
+				<img src="logo.jpg" title="Home" width="50" height="50" />
+			</a>
+				
+			<div class="dropdown_content">
+				<a href="about.php">About me</a>
+				<a href="update.php">Update</a>
+				<a href="profile.php">Profile</a>
+				<a href="contact.php">Contact me</a>
+			</div>
+				
+		</div>
+			
+		<div class="h_from_right">
+			<p id="clock">Loading</p>
+		</div>
+				
+	</div>	
+</header>
