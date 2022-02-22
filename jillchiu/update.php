97ab7482742b6749276af8191a835e8c30967e1e
jillchiu <99075985+jillchiu@users.noreply.github.com>
2022-02-23 05:21:51 +0900

    Update update.php

--- jillchiu/update.php
+++ jillchiu/update.php
@@ -18,6 +18,8 @@
 			<h2>Updated</h2>
 			<p>2022/02/12 Version 1.0 releases </p>
 			<p>2022/02/14 Pictures in about.php can be pop up now --Updated main.css, about.php, script.js</p>
+			<p>2022/02/15 Pictures in about.php in pop up mode can be zoom-in and zoom-out now --Updated main.css, about.php, script.js</p>
+			<p>2022/02/23 Pictures in index.php can be auto-loop, pop up, zoom-in and zoom-out now. ; Fix the miss in main.css and header.php --Updated header.php, index.php, main.css, script.js</p>
 			<h2>Going to update</h2>
 			<ol>
 				<li style="margin-left: 20px;">新功能 - 追加運用MYSQL部分(similar shopping cert?)</li>
@@ -26,6 +28,7 @@
 				<li style="margin-left: 20px;">改善 - navbar 向下拉時改成會消失</li>
 				<li style="margin-left: 20px;">改善 - 網頁整體隨視窗大小調整</li>
 				<li style="margin-left: 20px;">新功能 - 中, 英, 日版選項 at navbar</li>
+				<li style="margin-left: 20px;">新頁面 - portfolio.php 列出過去的作品</li>
 			</ol>
 		</div>
 	</div>
