68c0c7be1f11afd6d2ec5608f58b3b252a1fe968
jillchiu <99075985+jillchiu@users.noreply.github.com>
2022-05-15 21:56:20 +0900

    Add files via upload

---
+++ jillchiu 2.0/profile.php
@@ -0,0 +1,78 @@
+<?php include "header.php" ?>
+
+<title>Profile</title>
+<div class="background_layer_1">
+	<div class="background_layer_2">
+		<div class="background_layer_3">
+			<div class="background_layer_4">
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="common_main" style="background-color: white;">
+	<div class="profile_header">
+		<h1>Resume</h1><br/>
+		<center><hr/></center><br/>
+	</div>
+	<div class="profile_summary">
+		<dl>
+			<dt><h2>SUMMARY</h2></dt>
+			<dd><h3>Jill Chiu</h3></dd>
+			<dd><p>Majoring in <mark>Telecommunication and Networking</mark> when I was in institute</p></dd>
+			<dd><p>From Hong Kong</p></dd>
+			<dd><a href="mailto:jillll0329@gmail.com">jillll0329@gmail.com</a></dd>
+			<dd><a href="https://www.jillchiu.com">https://www.jillchiu.com</a></dd>
+		</dl>
+		<br/>
+	</div>
+	<div class="profile_experience">
+		<dl>
+			<dt><h2>EXPERIENCE</h2></dt>
+			<dd><p><mark>May 2017 - Present</mark></p></dd>
+			<dd><p>Clerk, at Hong Kong Government</p></dd>
+			<dd><p>Responsible for word processing</p></dd>
+		</dl>
+		<br/>
+	</div>
+	<div class="profile_education">
+		<dl>
+			<dt><h2>EDUCATION</h2></dt>
+			<dd><p><mark>Sep 2015 - June 2016</mark></p></dd>
+			<dd><p>Bachelor degree</p></dd>
+			<dd><p>Information Technology</p></dd>
+			<dd><p><mark>Sep 2013 - June 2015</mark></p></dd>
+			<dd><p>Higher Diploma</p></dd>
+			<dd><p>Telecommunication and Networking</p></dd>
+			<dd><p><mark>Sep 2006 - June 2012</mark></p></dd>
+			<dd><p>Secondary School</p></dd>
+			<dd><p>HKDSE Full cert</p></dd>
+		</dl>
+		<br/>
+	</div>
+	<div class="profile_skills">
+		<dl>
+			<dt><h2>SKILLS</h2></dt>
+			<dd><p>Experience on HTML, CSS, JavaScript(jQuery)</p></dd>
+			<dd><p>Experience on PHP, MySQL</p></dd>
+			<dd><p>Basic knowledge of Java, JSON, XML, fullpage, animate.min</p></dd>
+			<dd><p>Basic knowledge of troubleshooting, setting Router & Switch</p></dd>
+		</dl>
+		<br/>
+	</div>
+	<div class="profile_portfolios">
+		<dl>
+			<dt><h2>PORTFOLIOS</h2></dt>
+			<dd><p><mark>2022</mark></p></dd>
+			<dd><p><a href="https://jillchiu.000webhostapp.com/">My website</a></p></dd>
+			<dd><p>Using HTML, CSS, JavaScript(jQuery), PHP, MySQL</p></dd>
+			<dd><p><mark>2015</mark></p></dd>
+			<dd><p>Hotel Booking System (Bachelor Degree Final Year Project)</p></dd>
+			<dd><p>Using HTML, CSS, PHP, MySQL, JSON, XML</p></dd>
+		</dl>
+		<br/>
+	</div>
+	<center><p><mark>You can through <a href="contact.php">here</a> to contact me for full version resume.</mark></p></center>
+</div>
+
+<?php include "footer.php" ?>
---
+++ jillchiu 2.0/script.js
@@ -0,0 +1,368 @@
+/* index.php part */
+//index know me more & view source code button
+$(document).ready(function(){
+	$(".index_button").mouseover(function(){
+		$(this).css({"background-color":"#4CAF50",
+					"color":"white"});
+	});
+	$(".index_button").mouseout(function(){
+		$(this).css({"background-color":"#e7e7e7", 
+					"color":"black"});
+	});
+});
+
+//index pic arrow(left)
+$(function(){
+	$("#prev").click(function(){
+		if($("#pic123").attr("src")==="index_pic1.jpg"){
+			$("#pic123").attr("src", "index_pic3.jpg");
+			$("#btn1").attr("class", "null");
+			$("#btn3").attr("class", "light");
+		}else if($("#pic123").attr("src")==="index_pic2.jpg"){
+			$("#pic123").attr("src", "index_pic1.jpg");
+			$("#btn2").attr("class", "null");
+			$("#btn1").attr("class", "light");
+		}else if($("#pic123").attr("src")==="index_pic3.jpg"){
+			$("#pic123").attr("src", "index_pic2.jpg");
+			$("#btn3").attr("class", "null");
+			$("#btn2").attr("class", "light");
+		};
+	});
+});
+
+//index pic arrow(right)
+$(function(){
+	$("#next").click(function(){
+		switch($("#pic123").attr("src")){
+			case "index_pic1.jpg":
+				$("#pic123").attr("src", "index_pic2.jpg");
+				$("#btn1").attr("class", "null");
+				$("#btn2").attr("class", "light");
+				break;
+			case "index_pic2.jpg":
+				$("#pic123").attr("src", "index_pic3.jpg");
+				$("#btn2").attr("class", "null");
+				$("#btn3").attr("class", "light");
+				break;
+			case "index_pic3.jpg":
+				$("#pic123").attr("src", "index_pic1.jpg");
+				$("#btn3").attr("class", "null");
+				$("#btn1").attr("class", "light");
+				break;
+		};
+	});
+});
+
+//index buttons 切換
+$(function(){
+	
+	$("#btn1").click(function(){
+		switch($("#pic123").attr("src")){
+			case "index_pic1.jpg":
+				break;
+			case "index_pic2.jpg":
+				$("#btn2").attr("class", "null");
+				break;
+			case "index_pic3.jpg":
+				$("#btn3").attr("class", "null");
+				break;
+		};
+		$("#btn1").attr("class", "light");
+		$("#pic123").attr("src", "index_pic1.jpg");
+	});
+	
+	$("#btn2").click(function(){
+		switch($("#pic123").attr("src")){
+			case "index_pic1.jpg":
+				$("#btn1").attr("class", "null");
+				break;
+			case "index_pic2.jpg":
+				break;
+			case "index_pic3.jpg":
+				$("#btn3").attr("class", "null");
+				break;
+		};
+		$("#btn2").attr("class", "light");
+		$("#pic123").attr("src", "index_pic2.jpg");
+	});
+	
+	$("#btn3").click(function(){
+		switch($("#pic123").attr("src")){
+			case "index_pic1.jpg":
+				$("#btn1").attr("class", "null");
+				break;
+			case "index_pic2.jpg":
+				$("#btn2").attr("class", "null");
+				break;
+			case "index_pic3.jpg":
+				break;
+		};
+		$("#btn3").attr("class", "light");
+		$("#pic123").attr("src", "index_pic3.jpg");
+	});
+	
+});
+
+//index pictures auto play
+$(function(){
+	
+	//4sec/1pic
+	myInterval = setInterval(autoplay, 4000);
+	
+	function autoplay() {
+	  switch($("#pic123").attr("src")){
+			case "index_pic1.jpg":
+				$("#pic123").attr("src", "index_pic2.jpg");
+				$("#btn1").attr("class", "null");
+				$("#btn2").attr("class", "light");
+				break;
+			case "index_pic2.jpg":
+				$("#pic123").attr("src", "index_pic3.jpg");
+				$("#btn2").attr("class", "null");
+				$("#btn3").attr("class", "light");
+				break;
+			case "index_pic3.jpg":
+				$("#pic123").attr("src", "index_pic1.jpg");
+				$("#btn3").attr("class", "null");
+				$("#btn1").attr("class", "light");
+				break;
+		};
+	}
+	//when mouse on picture, stop loop
+	$("#pic123").mouseover(function(){
+		clearInterval(myInterval)
+	});
+	//when mouse left picture, restart loop
+	$("#pic123").mouseout(function(){
+	   myInterval = setInterval(autoplay, 4000)
+	});
+	
+});
+
+//popup picture control
+$(function(){
+	$("#pic123").click(function(){
+		$("#popup").css("display", "block");
+		$("#popup_img").attr("src", $(this).attr("src"));
+		
+		$("#close").click(function(){
+			$("#popup").css("display", "none");
+			$("#popup_img").attr("class", "zoom-in");
+		});
+		
+	});
+});
+
+//index my personality part1
+$(function(){
+	$("#subtitle_1").click(function(){
+		if($("#detail_1").css("display")==="none"){
+			$("#detail_1").css("display", "block");
+		}else{
+			$("#detail_1").css("display", "none");
+		};
+	});	
+});
+
+//index my personality part2
+$(function(){
+	$("#subtitle_2").click(function(){
+		$(".intro_part2_detail_2").toggle(1000);
+	});	
+});
+
+//index my personality part3
+$(function(){
+	$("#subtitle_3").click(function(){
+		if($("#detail_3").css("display")==="none"){
+			$("#detail_3").slideDown();
+		}else{
+			$("#detail_3").slideUp();
+		};
+	});	
+});
+
+/*
+function sub1(){
+	var x = document.getElementById("detail_1");
+	if (x.style.display==="none"){
+		x.style.display="block";
+	} else {
+		x.style.display="none";
+	}
+}
+*/
+
+/* header show clock */
+setInterval(function(){
+		
+		$date = new Date(); //Tue Jan 25 2022 12:15:50 GMT+0900 (日本標準時間)
+		$month = ($date.getMonth()<9)? "0"+($date.getMonth()+1) : ($date.getMonth()+1); //2022/02/01 23:38修正
+		$day = ($date.getDate()<10)? "0"+$date.getDate() : $date.getDate(); //2022/02/01 23:38修正
+		$hour = ""+$date.getHours();	//""未知
+		$minute = ($date.getMinutes()<10)? "0"+$date.getMinutes() : $date.getMinutes();	//""無效
+		$second = ($date.getSeconds()<10)? "0"+$date.getSeconds() : $date.getSeconds();	//""無效
+		
+		const $weeks = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+		$week = $weeks[$date.getDay()];
+		
+		$complete = $week + " " + $day + "/" + $month + "/" + $date.getFullYear() + " " + $hour + ":" + $minute + ":" + $second;
+		
+		$("#clock").text($complete);
+	
+}, 1000);
+	
+/* fullpage and animate.css control */
+$(function () {
+	$('#fullpage').fullpage({
+		navigation: true,
+		navigationPosition: "right",
+		navigationTooltips: ["page1", "page2", "page3", "page4", "page5"],
+		showActiveTooltip: true,
+		afterLoad: function(origin, destination){
+			
+			if(origin.index == 0){
+				$(".welcome").css('animation-delay', '.8s').addClass("animated zoomInDown");
+				$(".tips").css('animation-delay', '1s').addClass("animated bounceInUp");
+			}
+			
+		},
+		
+		onLeave: function(origin, destination){
+
+			$in_section = $(".section").eq(destination.index);
+			$out_section = $(".section").eq(origin.index);
+			
+			$(".welcome", $in_section).css('animation-delay', '.8s').addClass("animated zoomInDown");
+			$(".tips", $in_section).css('animation-delay', '1s').addClass("animated bounceInUp");
+			$(".appeal_left", $in_section).css('animation-delay', '.4s').addClass("animated fadeInLeftBig");
+			$(".appeal_right", $in_section).css('animation-delay', '.4s').addClass("animated fadeInRightBig");
+			$(".appeal_up", $in_section).css('animation-delay', '.4s').addClass("animated bounceInUp");
+			$(".appeal_down", $in_section).css('animation-delay', '.4s').addClass("animated rotateInDownLeft");
+			$(".appeal_link", $in_section).css('animation-delay', '.4s').addClass("animated flip");
+			
+			$(".welcome", $out_section).removeClass("animated zoomInDown");
+			$(".tips", $out_section).removeClass("animated bounceInUp");
+			$(".appeal_left", $out_section).removeClass("animated fadeInLeftBig");
+			$(".appeal_right", $out_section).removeClass("animated fadeInRightBig");
+			$(".appeal_up", $out_section).removeClass("animated bounceInUp");
+			$(".appeal_down", $out_section).removeClass("animated rotateInDownLeft");
+			$(".appeal_link", $out_section).removeClass("animated flip");
+		}
+	});
+});
+
+/* contact.php part */
+
+//回復輸入了的數據
+function callback($name, $email, $message){
+	$("#name").val($name);
+	$("#email").val($email);
+	$("#message").val($message);
+};
+
+//confirm before reset
+function reset_confirm(){
+	let text = "Are you sure clear all data you inputed ?";
+		if (confirm(text) == true){
+			document.getElementById("name").value = "";
+			document.getElementById("email").value = "";
+			document.getElementById("message").value = "";
+			//document.forms["contact"].reset();
+			$("#name").css("box-shadow", "");
+			$("#email").css("box-shadow", "");
+			$("#message").css("box-shadow", "");
+			alert("Data has been cleared.");
+		}else{
+			alert("Canceled.");
+		};
+};
+
+/*
+$(function(){
+	$("#reset").click(function(){
+		let $text = "Clear all?";
+		if (confirm($text) == true){
+			document.forms["contact"].reset();
+			alert("all cleared");
+		}else{
+			alert("canceled");
+		};
+	});
+});
+*/
+
+//check url, if = contact(.php), 離開頁面前提示輸入資料會消失 
+$(function(){
+	$position = $(location).attr("href");
+	$position_confirm = $position.search(/contact/);
+	
+	if($position_confirm != -1){
+		$(window).bind("beforeunload", function(){
+			
+			if(($("#name").val())!=="" || ($("#email").val())!=="" || ($("#message").val())!==""){
+				return "stop!";
+			};
+		
+		});
+	};
+});
+
+/* about.php part */
+
+//popup picture control
+$(function(){
+	$(".about_img").click(function(){
+		$("#popup").css("display", "block");
+		$("#popup_img").attr("src", $(this).attr("src"));
+		
+		$("#close").click(function(){
+			$("#popup").css("display", "none");
+			$("#popup_img").attr("class", "zoom-in");
+		});
+		
+	});
+});
+
+//after popup, can view the picture in limit size or original size
+$(function(){
+	$("#popup_img").click(function(){
+		if($(this).attr("class")=="zoom-in"){
+			$(this).attr("class", "zoom-out")
+		}else{
+			$(this).attr("class", "zoom-in")
+		}
+	});
+});
+
+/* portfolio.php part*/
+
+//fullpage & animate control
+$(function () {
+	$('#fullpage_portfolio').fullpage({
+		navigation: true,
+		navigationPosition: "right",
+		navigationTooltips: ["page1", "page2", "page3", "page4"],
+		showActiveTooltip: true,
+		sectionsColor : ['white', 'black', '#ccc', 'white'],
+		afterLoad: function(origin, destination){
+			
+			if(origin.index == 0){
+				$(".welcome_portfolio").css('animation-delay', '.8s').addClass("animated bounceIn");
+			}
+			
+		},
+		
+		onLeave: function(origin, destination){
+
+			$in_section = $(".section").eq(destination.index);
+			$out_section = $(".section").eq(origin.index);
+			
+			$(".welcome_portfolio", $in_section).css('animation-delay', '.8s').addClass("animated bounceIn");
+			$(".end_portfolio", $in_section).css('animation-delay', '1s').addClass("animated fadeInUp");
+			
+			$(".welcome_portfolio", $out_section).removeClass("animated bounceIn");
+			$(".end_portfolio", $out_section).removeClass("animated fadeInUp");
+		}
+	});
+});
